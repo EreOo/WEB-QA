0380b4541c794ae72c81b2fc617dbfb74f4f1e08
EreOo <Vladimircomment1995@gmail.com>
2018-05-06 17:17:33 +0300

    refactoring bad-test-example

--- src/test/java/OurFirstTest.java
+++ src/test/java/OurFirstTest.java
@@ -1,18 +1,19 @@
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.By;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
-public class TEST {
+public class OurFirstTest {
 
     @BeforeTest
     public void settings() {
         Configuration.browser = "chrome";
-        System.setProperty("selenide.browser", "chrome");
     }
 
     @Test
@@ -21,5 +22,10 @@ public class TEST {
         $(By.cssSelector("#header-search > div > form > div > input")).setValue("qweqwe");
         $(By.cssSelector("#header-search > div > form > div > span.input-group-btn > button")).click();
         $(By.id("empty-search-results")).shouldHave(text("К сожалению, по запросу «qweqwe» мы ничего не смогли найти."));
+    }
+
+    @AfterTest
+    public void close() {
+        getWebDriver().close();
     }
 }
