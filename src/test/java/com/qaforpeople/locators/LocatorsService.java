9a7c44c713e30279a607fe57524aaaed45e84b3f
vshekhavtsov <vshekhavts@wiley.com>
2018-05-11 19:50:59 +0300

    last commit worked!!!

--- src/test/java/com/qaforpeople/locators/LocatorsService.java
+++ src/test/java/com/qaforpeople/locators/LocatorsService.java
@@ -3,6 +3,7 @@ package com.qaforpeople.locators;
 import com.qaforpeople.locators.interfaces.InputFieldsBlockLocators;
 import com.qaforpeople.locators.interfaces.MainPageLocators;
 import com.qaforpeople.locators.interfaces.SecondPageLocators;
+import com.qaforpeople.locators.platforms.MobileMainPageLocators;
 import com.qaforpeople.locators.platforms.WebInputFieldsBlockLocators;
 import com.qaforpeople.locators.platforms.WebMainPageLocators;
 import com.qaforpeople.locators.platforms.WebSecondPageLocators;
@@ -14,7 +15,8 @@ import com.qaforpeople.locators.platforms.WebSecondPageLocators;
  public final static MainPageLocators ANOTHER_PAGE_LOCATORS = System.getProperty("platform.env").equals("pc") ? new WebAnotherPageLocators() : new MobileAnotherPageLocators();
  */
 public class LocatorsService {
-    public static final MainPageLocators MAIN_PAGE_LOCATORS = new WebMainPageLocators();
+    public static final MainPageLocators MAIN_PAGE_LOCATORS = System.getProperty("platform.env")
+            .equals("pc") ? new WebMainPageLocators() : new MobileMainPageLocators();
     public static final InputFieldsBlockLocators INPUT_FIELDS_BLOCK_LOCATORS = new WebInputFieldsBlockLocators();
     public static final SecondPageLocators TEXT_PAGE_LOCATORS = new WebSecondPageLocators();
 
