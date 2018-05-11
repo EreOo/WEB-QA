1040756859779cd71ac096852f2ff3c23d27c412
vshekhavtsov <vshekhavts@wiley.com>
2018-05-11 18:20:51 +0300

    fix Before suite settings

--- src/test/java/com/qaforpeople/base/BaseTest.java
+++ src/test/java/com/qaforpeople/base/BaseTest.java
@@ -2,14 +2,19 @@ package com.qaforpeople.base;
 
 import com.codeborne.selenide.Configuration;
 import com.qaforpeople.pages.MainPage;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
@@ -22,7 +27,6 @@ public class BaseTest {
     private static final String SITE_URL = "https://ereoo.github.io/main-page";
 
     protected MainPage openSite() {
-        selectBrowser();
         open(SITE_URL);
         return new MainPage();
     }
@@ -30,6 +34,7 @@ public class BaseTest {
     /**
      * set property "browser" from pom.xml
      */
+    @BeforeSuite
     private void selectBrowser() {
         //It's localhost - but jenkins in container and it need your local ip
         Configuration.remote = "http://169.254.243.36:4444/wd/hub";
