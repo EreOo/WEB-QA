43fe40b5587ed5c2a9ab34e0c79c642cbc6e3488
vshekhavtsov <vshekhavts@wiley.com>
2018-05-11 18:35:53 +0300

    sorry mom :C

--- src/test/java/com/qaforpeople/base/BaseTest.java
+++ src/test/java/com/qaforpeople/base/BaseTest.java
@@ -2,22 +2,8 @@ package com.qaforpeople.base;
 
 import com.codeborne.selenide.Configuration;
 import com.qaforpeople.pages.MainPage;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 /**
  * Author Vladimir S
@@ -27,6 +13,7 @@ public class BaseTest {
     private static final String SITE_URL = "https://ereoo.github.io/main-page";
 
     protected MainPage openSite() {
+        selectBrowser();
         open(SITE_URL);
         return new MainPage();
     }
@@ -34,7 +21,6 @@ public class BaseTest {
     /**
      * set property "browser" from pom.xml
      */
-    @BeforeSuite
     private void selectBrowser() {
         //It's localhost - but jenkins in container and it need your local ip
         Configuration.remote = "http://169.254.243.36:4444/wd/hub";
