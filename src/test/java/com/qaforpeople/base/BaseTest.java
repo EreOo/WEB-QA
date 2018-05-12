7a8330873c4422668ec18c157997b3bd6e72a13e
EreOo <Vladimircomment1995@gmail.com>
2018-05-12 15:24:07 +0300

    fix select browser and close driver.

--- src/test/java/com/qaforpeople/base/BaseTest.java
+++ src/test/java/com/qaforpeople/base/BaseTest.java
@@ -2,10 +2,9 @@ package com.qaforpeople.base;
 
 import com.codeborne.selenide.Configuration;
 import com.qaforpeople.pages.MainPage;
-import org.testng.annotations.AfterTest;
+import org.testng.annotations.AfterSuite;
 
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 /**
  * Author Vladimir S
@@ -15,7 +14,6 @@ public class BaseTest {
     private static final String SITE_URL = "https://ereoo.github.io/main-page";
 
     protected MainPage openSite() {
-        selectBrowser();
         open(SITE_URL);
         return new MainPage();
     }
@@ -23,17 +21,8 @@ public class BaseTest {
     /**
      * set property "browser" from pom.xml
      */
+    @AfterSuite
     private void selectBrowser() {
         Configuration.browser = System.getProperty("browser");
-    }
-
-//    TODO delete it.
-    /**
-     * Selenide has shutdown driver method but I have issue:
-     * Browsers don't close and CPU're working 100% after few test runs.
-     */
-    @AfterTest
-    public void closeDriver() {
-        getWebDriver().close();
     }
 }
