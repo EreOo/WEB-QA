6e179c084ef26484cf38ea6c76a349a7a10c1668
vshekhavtsov <vshekhavts@wiley.com>
2018-05-14 11:37:19 +0300

    fix

--- src/test/java/com/qaforpeople/base/BaseTest.java
+++ src/test/java/com/qaforpeople/base/BaseTest.java
@@ -25,13 +25,15 @@ public class BaseTest {
     private void selectBrowser() {
         //It's localhost - but jenkins in container and it need your local ip
         Configuration.remote = "http://169.254.243.36:4444/wd/hub";
-        Configuration.browser = System.getProperty("browser");
+        Configuration.browser = System.getenv("BROWSER");
+        System.setProperty("platform.env", System.getenv("PLATFORM"));
         System.setProperty("platform", "MAC");
 
         System.out.println("!!!!!!!!!!____________" + Configuration.remote + "__________!!!!!!!!!!!!!!");
         System.out.println("!!!!!!!!!!____________" + Configuration.browser + "__________!!!!!!!!!!!!!!");
         System.out.println("!!!!!!!!!!____________" + System.getProperty("browser") + "__________!!!!!!!!!!!!!!");
         System.out.println("!!!!!!!!!!____________" + System.getProperty("platform") + "__________!!!!!!!!!!!!!!");
+        System.out.println("!!!!!!!!!!____________" + System.getProperty("platform.env") + "__________!!!!!!!!!!!!!!");
 
 
     }
