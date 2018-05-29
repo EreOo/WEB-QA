8fc1df05fe88f86d587673b422ec4da4fc925b14
EreOo <Vladimircomment1995@gmail.com>
2018-05-29 19:29:21 +0300

    Remote driver for Jenkins

--- src/test/java/com/qaforpeople/base/BaseTest.java
+++ src/test/java/com/qaforpeople/base/BaseTest.java
@@ -3,10 +3,9 @@ package com.qaforpeople.base;
 import com.codeborne.selenide.Configuration;
 import com.qaforpeople.pages.MainPage;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 
+import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 /**
  * Author Vladimir S
@@ -16,20 +15,21 @@ public class BaseTest {
     private static final String SITE_URL = "https://ereoo.github.io/main-page";
 
     protected MainPage openSite() {
+        selectBrowser();
         open(SITE_URL);
         return new MainPage();
     }
 
     /**
-     * set property "browser" from pom.xml
+     * set property from pom.xml
      */
-    @BeforeTest
     private void selectBrowser() {
         Configuration.browser = System.getProperty("browser");
+        Configuration.remote = System.getProperty("hub");
     }
 
     @AfterTest
-    private void close() {
-        getWebDriver().close();
+    private void closeBrowser() {
+        close();
     }
 }
